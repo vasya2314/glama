249939467119eec0f82080dd5232f86206577615
Vailiy <skorov@grampus-studio.ru>
2024-06-20 08:48:53 +0300

    UPDATE

--- app/Models/ClosingAct.php
+++ app/Models/ClosingAct.php
@@ -2,20 +2,28 @@
 
 namespace App\Models;
 
+use App\Observers\ClosingActObserver;
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
+#[ObservedBy([ClosingActObserver::class])]
 class ClosingAct extends Model
 {
     use HasFactory;
 
-    protected $table = 'closing_receipts';
+    protected $table = 'closing_acts';
     protected $guarded = false;
 
-    public function clients(): BelongsTo
+    public function contract(): BelongsTo
     {
-        return $this->belongsTo(Client::class);
+        return $this->belongsTo(Contract::class);
+    }
+
+    public function closingDocument(): HasOne
+    {
+        return $this->hasOne(ClosingDocument::class);
     }
 }
