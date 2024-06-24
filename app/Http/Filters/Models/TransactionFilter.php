b4964cfad72abb8eddccb4c19e578bdc87427f0b
Vailiy <skorov@grampus-studio.ru>
2024-06-24 14:51:36 +0300

    UPDATE

--- app/Http/Filters/Models/TransactionFilter.php
+++ app/Http/Filters/Models/TransactionFilter.php
@@ -10,6 +10,7 @@ class TransactionFilter extends AbstractFilter
     public const ACCOUNT_NAME = 'type';
     public const DATE_START = 'date_start';
     public const DATE_END = 'date_end';
+    public const METHOD_TYPE = 'method_type';
 
     protected function getCallbacks(): array
     {
@@ -17,12 +18,18 @@ class TransactionFilter extends AbstractFilter
             self::ACCOUNT_NAME => [$this, 'type'],
             self::DATE_START => [$this, 'dateStart'],
             self::DATE_END => [$this, 'dateEnd'],
+            self::METHOD_TYPE => [$this, 'methodType'],
         ];
     }
 
     public function type(Builder $builder, $value): void
     {
         $builder->where('type', 'like', "%{$value}%");
+    }
+
+    public function methodType(Builder $builder, $value): void
+    {
+        $builder->where('method_type', 'like', "%{$value}%");
     }
 
     public function dateStart(Builder $builder, $value): void
